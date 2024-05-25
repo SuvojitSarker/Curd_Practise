826297df181b811d46eff5a8653173517f3a3783
SuvojitSarker <suvojit460159@bsdi-bd.org>
2024-05-26 03:55:09 +0600

    api added  with get methode

--- lib/add_new_item_screen.dart
+++ lib/add_new_item_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class AddNewProductItemScreen extends StatefulWidget {
   const AddNewProductItemScreen({super.key});
@@ -15,6 +18,7 @@ class _AddNewProductItemScreenState extends State<AddNewProductItemScreen> {
   final TextEditingController _quantityTEController = TextEditingController();
   final TextEditingController _totalPriceTEController = TextEditingController();
   final TextEditingController _imageTEController = TextEditingController();
+  final TextEditingController _productCodeTEController = TextEditingController();
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
   @override
@@ -55,6 +59,21 @@ class _AddNewProductItemScreenState extends State<AddNewProductItemScreen> {
                   validator: (String? value){
                     if(value==null || value.trim().isEmpty){
                       return 'Write Your Produce Price';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16,),
+                TextFormField(
+                  controller: _productCodeTEController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                      hintText: 'Product Code',
+                      labelText: 'Product Code'
+                  ),
+                  validator: (String? value){
+                    if(value==null || value.trim().isEmpty){
+                      return 'Write Your Product Code';
                     }
                     return null;
                   },
@@ -108,6 +127,7 @@ class _AddNewProductItemScreenState extends State<AddNewProductItemScreen> {
                 ElevatedButton(
                     onPressed: () {
                       if(_formkey.currentState!.validate()){
+                        _addProduct();
                       }
                     }, child: Text('Add'),),
               ],
@@ -117,6 +137,37 @@ class _AddNewProductItemScreenState extends State<AddNewProductItemScreen> {
       ),
     );
   }
+
+  Future<void> _addProduct() async {
+    //step 1 : set url
+    const String addNewProductUrl =
+        'https://crud.teamrabbil.com/api/v1/CreateProduct';
+    //step 2 : set data
+    Map<String, dynamic> inputData = {
+      "Img": _imageTEController.text.trim(),
+      "ProductCode": _productCodeTEController.text,
+      "ProductName": _nameTEController.text,
+      "Qty": _quantityTEController.text,
+      "TotalPrice": _totalPriceTEController.text,
+      "UnitPrice": _unitTEController.text,
+    };
+    //step 3 : parse
+    Uri uri = Uri.parse(addNewProductUrl);
+    //step 4 : Send Request
+    Response response = await post(
+      uri,
+      body: jsonEncode(inputData),
+      headers: {'content-type': 'application/json'},
+    );
+    print(response.statusCode);
+    print(response.body);
+    print(response.headers);
+  }
+
+  //TODO : From clear after add
+  //TODO : AddButton should be in progress after click (loading)
+  //TODO : Toast / SnackBar
+
 
   @override
   void dispose() {
